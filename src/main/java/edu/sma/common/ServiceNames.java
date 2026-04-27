8d06845c0d297ec2d9538f91d21fc5b1780aec6d
KarloToro <135350154+KarloToro@users.noreply.github.com>
2026-04-27 13:13:10 -0500

    refactor utilidades

--- src/main/java/edu/sma/common/ServiceNames.java
+++ src/main/java/edu/sma/common/ServiceNames.java
@@ -1,10 +1,9 @@
 package edu.sma.common;
 
 public final class ServiceNames {
-    public static final String NOVIO_SERVICE = "novio-service";
+
     public static final String RECEPCIONISTA_SERVICE = "recepcionista-service";
     public static final String FLORISTA_SERVICE = "florista-service";
-
     private ServiceNames() {
     }
 }
